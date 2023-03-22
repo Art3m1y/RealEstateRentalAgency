b181862b3f5fab6e45c3bbfc844c4c3e5f46aac3
Artem Kuryshkin <70939390+Art3m1y@users.noreply.github.com>
2023-03-22 18:58:11 +0300

    Created backend folder

---
+++ backend/src/main/java/ru/Art3m1y/RealEstateRentalAgency/RealEstateRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.Art3m1y.RealEstateRentalAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateRentalServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/Art3m1y/RealEstateRentalAgency/RealEstateRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Art3m1y.RealEstateRentalAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
